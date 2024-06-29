e189eb6b5a19f371e39b743ad2bdb3f946e90097
Giovanni Romano <romanogiovanni1998@gmail.com>
2024-06-29 22:41:28 +0200

    bugfix

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ function getCookie(name) {
     let ca = decodedCookie.split(';');
     for(let i = 0; i < ca.length; i++) {
         let c = ca[i];
-        while (c.charAt(0) == ' ') {
+        while (c.charAt(0) === ' ') {
             c = c.substring(1);
         }
-        if (c.indexOf(cname) == 0) {
+        if (c.indexOf(cname) === 0) {
             return c.substring(cname.length, c.length);
         }
     }
